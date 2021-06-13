f43e15090ed0f556bd82bb374bbf4405dea62010
Luis Peçanha <luis@dsbrigade.com>
2021-06-13 20:38:13 -0300

    Fix election round
    Forgot to include the first round of the election in task 2.

--- project_1/2.sql
+++ project_1/2.sql
@@ -8,6 +8,7 @@ FROM candmz_m2018
 LEFT JOIN brestados ON candmz_m2018.sg_uf = brestados.uf
 WHERE brestados.regiao = 'Sudeste'
   AND candmz_m2018.ds_cargo = 'Presidente'
+  AND candmz_m2018.nr_turno = 1
 GROUP BY candmz_m2018.nm_candidato
 ORDER BY total_de_votos_obtidos DESC
 LIMIT 5;
